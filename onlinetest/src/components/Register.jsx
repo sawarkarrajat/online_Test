82ad36584178b48ee50ad8cdb8351cbe29ee27a4
Rajat Sawarkar <rajat.sawarkar@office365.3pillarglobal.com>
2020-02-18 13:41:35 +0530

    finished with registration minification

--- onlinetest/src/components/Register.jsx
+++ onlinetest/src/components/Register.jsx
@@ -1,120 +1,126 @@
 import React from "react";
 import "../sass/Register.sass";
-import Button from '@material-ui/core/Button';
-import { ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
-import * as data from "../data/..";
-const fs = require('fs');
-
+import Button from "@material-ui/core/Button";
+import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
+// import questions from "../data/questions.json";
+const txt = ["R", "E", "G", "I", "S", "T", "E", "R"];
 
 class Register extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       user: {
-        name:'',
-        email: '',
-        password: '',
-        repeatPassword: '',
-    },
+        name: "",
+        email: "",
+        password: "",
+        repeatPassword: ""
+      }
     };
   }
-  
+
   componentDidMount() {
     // custom rule will have name 'isPasswordMatch'
-    ValidatorForm.addValidationRule('isPasswordMatch', (value) => {
-        if (value !== this.state.user.password) {
-            return false;
-        }
-        return true;
+    ValidatorForm.addValidationRule("isPasswordMatch", value => {
+      if (value !== this.state.user.password) {
+        return false;
+      }
+      return true;
     });
-  //   fs.readFileSync(data,(err, jsonString) => {
-  //     if (err) {
-  //         console.log("File read failed:", err)
-  //         return
-  //     }
-  //     console.log('File data:', jsonString) 
-  // })
-}
+  }
 
-componentWillUnmount() {
+  componentWillUnmount() {
     // remove rule when it is not needed
-    ValidatorForm.removeValidationRule('isPasswordMatch');
-}
+    ValidatorForm.removeValidationRule("isPasswordMatch");
+  }
 
-handleChange = (event) => {
+  handleChange = event => {
     const { user } = this.state;
+    console.log("value is",event.target.value);
+    
     user[event.target.name] = event.target.value;
-    this.setState({ user });
-}
+    this.setState({ user },()=>{
+      console.log("state is",this.state);
+      
+    });
+  };
 
-handleSubmit = () => {
+  handleSubmit = () => {
     // your submit logic
-}
+  };
   render() {
-    const { user } = this.state;
-    return ( 
-        <div className="sbody ">
-          <div id="regForm">
-            {/* <div className="bg1"></div>
-            <div className="bg2"></div> */}
+    // const { user } = this.state;
+    const fields = [
+      {
+        label: "Name",
+        name: "name",
+        value: this.state.user.name,
+        validators: ["required"],
+        em: ["this field is required"],
+        helperText: "Enter first name and last name with a space"
+      },
+      {
+        label: "Email",
+        name: "email",
+        value: this.state.user.email,
+        validators: ["required", "isEmail"],
+        em: ["this field is required", "email is not valid"],
+        helperText: ""
+      },
+      {
+        label: "Password",
+        name: "password",
+        value: this.state.user.password,
+        validators: ["required"],
+        em: ["this field is required"],
+        helperText: ""
+      },
+      {
+        label: "Repeat password",
+        name: "repeatPassword",
+        value: this.state.user.repeatPassword,
+        validators: ["isPasswordMatch", "required"],
+        em: ["password mismatch", "this field is required"],
+        helperText: ""
+      }
+    ];
+    return (
+      <div className="sbody ">
+        <div id="regForm">
           <ValidatorForm
-                ref="form"
-                onSubmit={this.handleSubmit}
-                onError={errors => console.log(errors)}
+            ref="form"
+            onSubmit={this.handleSubmit}
+            onError={errors => console.log(errors)}
+          >
+            {fields.map((data,index) => (
+              <TextValidator
+                key={index+121}
+                label={data.label}
+                onChange={this.handleChange}
+                fullWidth
+                name={data.name}
+                value={data.value}
+                validators={data.validators}
+                errorMessages={data.em}
+                helperText={data.helperText}
+              />
+            ))}
+
+            <Button
+              id="submit"
+              variant="contained"
+              color="primary"
+              type="submit"
             >
-              <TextValidator
-                    label="Name"
-                    onChange={this.handleChange}
-                    fullWidth
-                    name="name"
-                    value={user.name}
-                    validators={['required']}
-                    errorMessages={['this field is required']}
-                    helperText="Enter first name and last name with a space"
-                />
-              <TextValidator
-                    label="Email"
-                    onChange={this.handleChange}
-                    fullWidth
-                    name="email"
-                    value={user.email}
-                    validators={['required', 'isEmail']}
-                    errorMessages={['this field is required', 'email is not valid']}
-                />
-                <TextValidator
-                    label="Password"
-                    onChange={this.handleChange}
-                    fullWidth
-                    name="password"
-                    type="password"
-                    validators={['required']}
-                    errorMessages={['this field is required']}
-                    value={user.password}
-                />
-                <TextValidator
-                    label="Repeat password"
-                    onChange={this.handleChange}
-                    fullWidth
-                    name="repeatPassword"
-                    type="password"
-                    validators={['isPasswordMatch', 'required']}
-                    errorMessages={['password mismatch', 'this field is required']}
-                    value={user.repeatPassword}
-                />
-                <Button variant="contained" color="primary" type="submit">Submit</Button>
-            </ValidatorForm>
-          </div>
-          <div id="sideText">
-            <span>R</span>
-            <span>E</span>
-            <span>G</span>
-            <span>I</span>
-            <span>S</span>
-            <span>T</span>
-            <span>E</span>
-            <span>R</span>
-          </div>
+              Submit
+            </Button>
+          </ValidatorForm>
         </div>
+        <div id="sideText">
+          {txt.map((alpha,index) => (
+            <span key={index}>{alpha}</span>
+          ))}
+        </div>
+      </div>
     );
   }
 }
--- onlinetest/src/dump.jsx
+++ onlinetest/src/dump.jsx
@@ -1,6 +1,41 @@
-// #49e5ff
-// #6a5edf
-// #000000
-// "template": "./custom-template",
-//     "tutorials": "./tutorials",
-//     "readme": "./readme/readme.md"
+// // #49e5ff
+// // #6a5edf
+// // #000000
+// // "template": "./custom-template",
+// //     "tutorials": "./tutorials",
+// //     "readme": "./readme/readme.md"
+
+// // {questions.map((data,index)=>{
+
+// //   return <h4 key={data.sno}>{data.question}</h4>
+// // })}
+
+// <TextValidator
+//               label="Email"
+//               onChange={this.handleChange}
+//               fullWidth
+//               name="email"
+//               value={user.email}
+//               validators={["required", "isEmail"]}
+//               errorMessages={["this field is required", "email is not valid"]}
+//             />
+//             <TextValidator
+//               label="Password"
+//               onChange={this.handleChange}
+//               fullWidth
+//               name="password"
+//               type="password"
+//               validators={["required"]}
+//               errorMessages={["this field is required"]}
+//               value={user.password}
+//             />
+//             <TextValidator
+//               label="Repeat password"
+//               onChange={this.handleChange}
+//               fullWidth
+//               name="repeatPassword"
+//               type="password"
+//               validators={["isPasswordMatch", "required"]}
+//               errorMessages={["password mismatch", "this field is required"]}
+//               value={user.repeatPassword}
+//             />
